b366b81e5631ef2e762712cce791c05f1ec66276
tomoya hirashima <hirashi.dev@gmail.com>
2025-05-22 05:14:35 +0900

    header修正

--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -9,6 +9,20 @@
   <?php wp_head(); ?>
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MF7J5CR3PX"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'G-MF7J5CR3PX');
+</script>
+
+
 <body>
   <header class="header js-header" id="#js-header">
     <div class="header__inner">
